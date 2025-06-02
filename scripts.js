329700e4a03e218c3219b5b2838a9b5767808fa4
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2025-06-02 14:59:50 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
 // Scripts will go here
+$(document).ready(function () {
+  function writeData() {
+    const db = getDatabase();
+    set(ref(db, 'hello/'), {
+      world: 'world';
+    });
+  }
+  writeData();
+});

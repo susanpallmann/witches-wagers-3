0519403ea5a602e9e7332681330dcb5ad9953232
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2025-06-02 15:33:30 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 // Scripts will go here
-$(document).ready(function () {
-  import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/11.8.0/firebase-database.js';
+import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/11.8.0/firebase-database.js';
   
-  function writeData() {
-    const db = getDatabase();
-    set(ref(db, 'test'), {
-      test: 'test'
-    });
-  }
+function writeData() {
+  const db = getDatabase();
+  set(ref(db, 'test'), {
+    test: 'test'
+  });
+}
+$(document).ready(function () {
   writeData();
 });

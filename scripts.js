20f0c4f946ff9292a8dd4eb0532c2d7c3ff851d5
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2025-06-02 15:48:22 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/11.8.0/firebase-database.js';
-import { getAuth, signInAnonymously } from 'https://www.gstatic.com/firebasejs/11.8.0/firebase-auth.js';
+import { getAuth, setPersistence, browserSessionPersistence, signInAnonymously } from 'https://www.gstatic.com/firebasejs/11.8.0/firebase-auth.js';
 
 function writeData() {
   const db = getDatabase();
@@ -19,6 +19,20 @@ function signInAnon() {
     const errorCode = error.code;
     const errorMessage = error.message;
     // ...
+  });
+  setPersistence(auth, browserSessionPersistence)
+  .then(() => {
+    // Existing and future Auth states are now persisted in the current
+    // session only. Closing the window would clear any existing state even
+    // if a user forgets to sign out.
+    // ...
+    // New sign-in will be persisted with session persistence.
+      return signInAnonymously(auth);
+    })
+  .catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
   });
 }
 

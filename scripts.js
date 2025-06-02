6ea4126ba661c73551c0ca0ccfc67b706f9187dd
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2025-06-02 16:19:41 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -22,17 +22,17 @@ function signInAnon() {
       // ...
     });
   onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/auth.user
-    const uid = user.uid;
-    console.log(uid);
-    // ...
-  } else {
-    // User is signed out
-    // ...
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/auth.user
+      const uid = user.uid;
+      console.log(uid);
+      // ...
+    } else {
+      // User is signed out
+      // ...
+    }
   }
-}
 }
 
 $(document).ready(function () {

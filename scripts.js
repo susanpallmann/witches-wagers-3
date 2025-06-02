8d1927374a2da597e05c94cad96825ee8209fa4d
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2025-06-02 16:55:18 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@ import { getDatabase, ref, set, onValue } from 'https://www.gstatic.com/firebase
 import { getAuth, onAuthStateChanged, signInAnonymously} from 'https://www.gstatic.com/firebasejs/11.8.0/firebase-auth.js';
 
 function writeData(uid) {
-  let uid = uid;
   const db = getDatabase();
   set(ref(db, 'Rooms/TEST/players/' + uid), {
     test: 'test'

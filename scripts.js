2bf35bc293ae0bbf353e6235c78f9de026b1708d
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2025-06-02 15:38:33 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,28 @@
 import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/11.8.0/firebase-database.js';
-  
+import { getAuth, signInAnonymously } from 'https://www.gstatic.com/firebasejs/11.8.0/firebase-auth.js';
+
 function writeData() {
   const db = getDatabase();
   set(ref(db, 'test'), {
     test: 'test'
   });
 }
+
+function signInAnon() {
+  const auth = getAuth();
+  signInAnonymously(auth)
+  .then(() => {
+    // Signed in..
+    console.log('signed in successfully');
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ...
+  });
+}
+
 $(document).ready(function () {
   writeData();
+  signInAnon();
 });

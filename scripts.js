b254853d35294d26e667fabfdc8ff69dc3fbaa00
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2025-06-02 16:44:16 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -26,7 +26,7 @@ function signInAnon() {
       // User is signed in, see docs for a list of available properties
       // https://firebase.google.com/docs/reference/js/auth.user
       const uid = user.uid;
-      auth.setCustomUserClaims(uid, { hostScreen: true })
+      setCustomUserClaims(uid, { hostScreen: true })
       .then(() => {
       });
       console.log(uid);

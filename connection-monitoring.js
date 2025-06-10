ae136bcef30c2fbd7c195b4b69f01d9b9c6b752f
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2025-06-10 19:49:09 -0400

    Update connection-monitoring.js

--- connection-monitoring.js
+++ connection-monitoring.js
@@ -1,332 +1,282 @@
-import { getDatabase, ref, get, child, set, onValue, push, update } from 'https://www.gstatic.com/firebasejs/11.8.0/firebase-database.js';
+import { getDatabase, ref, get, child, set, onValue, push, update, remove } from 'https://www.gstatic.com/firebasejs/11.8.0/firebase-database.js';
 import { getAuth, onAuthStateChanged, signInAnonymously} from 'https://www.gstatic.com/firebasejs/11.8.0/firebase-auth.js';
 
-const verificationCadence = 60000;
-const minimumGuests = 1;
+const ageAllowance = 60000;
+const minGuests = 4
+const maxGuests = 8;
 
-function removeGuests(guests) {
-  return new Promise(resolve => {
+class userSession = {
+	constructor(uid) {
+		this.uid = uid;
+	}
+}
+
+
+function isUserHost(user) {
+	return user.host;
+}
+
+function removeUsers(disconnectedUsers) {
     const db = getDatabase();
-    for (let i = 0; i < guests.length; i++) {
-      let guest = guests[i];
-      set(ref(db, `rooms/TEST/connection/users/${guest}`), {
-        null: null
-      }).then(() => {
-      }).catch((error) => {
-        console.log('removeGuests: ' + error);
-        resolve(false);
-      });
-    }
-    resolve(true);
-  });
+    const removalPromises = disconnectedUsers.map(user => {
+        return remove(ref(db, `rooms/TEST/connection/users/${user.id}`));
+    });
+    // Just return the promise that Promise.all creates
+    return Promise.all(removalPromises);
+}
+
+function writeDisconnectCode(data) {
+    return new Promise(function (resolve, reject) {
+        const { disconnectedUsers, users } = data;
+
+        // 1. First, check if the host is among the disconnected. This is the highest priority.
+        const isHostDisconnected = disconnectedUsers.some(user => isUserHost(user));
+        if (isHostDisconnected) {
+            resolve('hostDisconnected');
+            return; // Exit the function
+        }
+
+        // 2. Next, check if removing these users would drop the lobby below the minimum required players.
+        const remainingUserCount = Object.keys(users).length - disconnectedUsers.length;
+        if (remainingUserCount < minGuests) {
+            resolve('notEnoughGuests');
+            return; // Exit the function
+        }
+        
+        // 3. If neither of the above is true, the default action is to remove the guests.
+        // We can add a sanity check here to ensure we have disconnected users to remove.
+        if (disconnectedUsers.length > 0) {
+            resolve('removeGuests');
+            return; // Exit the function
+        }
+
+        // This case should theoretically not be hit if handleDisconnectedUsers is working,
+        // but it's good practice to handle all paths.
+        reject(`writeDisconnectCode: a disconnect code should not be created for this situation; see data (${data}).`);
+    });
+}
+
+
+// Function that gets a snapshot of user data from the lobby's 'connection' child.
+function getConnectedUserData() {
+	return new Promise(function (resolve, reject) {
+		
+		// Get our database reference.
+		let db = getDatabase();
+		
+		// Take a snapshot of our lobby's 'connection' child.
+		get(ref(db, 'rooms/TEST/connection')).then((snapshot) => {
+			let users = snapshot.val().users;
+			
+			// Return the 'users' object containing all users if successful.
+			resolve(users);
+			
+		// Return a Firebase error if we are unable to get the snapshot.
+		}).catch((error) => {
+			reject(`getConnectedUserData: Firebase error: ${error}`);
+		});
+	});
+}
+
+// Function that, given a list of users, checks if any users have disconnected and returns an array of users that have disconnected. Returns an error if any users have check ins dated in the future.
+function getDisconnectedUsers(users) {
+	return new Promise(function(resolve, reject) {
+		
+		// Get the current time as a timestamp
+		let refTimestamp = Date.now();
+		
+		// Prepare an array for storing disconnected users
+		let disconnectedUsers = [];
+		
+		// Check each user in our users object
+		for (let user in users) {
+			
+			// Get the user's last verification timestamp
+			let userTimestamp = users[user].lastVerified;
+			
+			// If the user's timestamp is less than the current time minus a preset ageAllowance (the maximum amount of time a user can go without verifying to still be considered connected), add that user to the disconnectedUsers array.
+			if (userTimestamp  <= refTimestamp - ageAllowance) {
+				disconnectedUsers.push({
+					id: user, ...users[user]
+				});
+				
+			// If the user's timestamp is in the future, return an error.
+			} else if (userTimestamp  > refTimestamp) {
+				reject(`getDisconnectedUsers: userTimestamp (${userTimestamp}) exceeds refTimestamp (${refTimestamp}).`);
+			
+			// Otherwise, do not add the user to the disconnectedUsers array.
+			} else {
+			}
+		}
+		
+		// Once all users have been checked, send the users object and disconnectedUsers array (this may be empty).
+			resolve({
+				users: users,
+				disconnectedUsers : disconnectedUsers
+		});
+	});
 }
 
 function updateConnectionStatus(code) {
-  return new Promise(resolve => {
-    const db = getDatabase();
-    update(ref(db, 'rooms/TEST/connection'), {
-      connectionStatus: code
-    })
-    .then(() => {
-      resolve(true);
-    })
-    .catch((error) => {
-      console.log('updateConnectionStatus: ' + error);
-      resolve(false);
-    });
-  });
+	return new Promise(function (resolve, reject) {
+		const db = getDatabase();
+		update(ref(db, 'rooms/TEST/connection'), {
+		  connectionStatus: code
+		})
+		.then(() => {
+		  resolve(true);
+		})
+		.catch((error) => {
+		  reject(`updateConnectionStatus: Firebase error: ${error}`);
+		});
+
+	}
 }
 
-
-async function missingCheckIn(code, users) {
-  return new Promise(async resolve => {
-    if (code === 'hostDisconnect' || code === 'notEnoughGuests') {
-      let updateSuccessful = await updateConnectionStatus(code);
-      if (updateSuccessful) {
-        console.log('missingCheckIn: Updated connectionStatus to ' + code + ' successfully.');
-        resolve(true);
-      } else {
-        console.log('missingCheckIn: Unable to update connectionStatus to ' + code);
-        resolve(false);
-      }
-    } else if (code === 'removeGuests') {
-      if (users !== null) {
-        console.log(users);
-        let updateSuccessful = await removeGuests(users);
-        if (updateSuccessful) {
-          console.log('missingCheckIn: Removed disconnected guests ' + users.join(', ') + ' successfully.');
-          resolve(true);
-        }
-      } else {
-        console.log('missingCheckIn: Missing required parameter: users.');
-        resolve(false);
-      }
-    } else {
-      console.log('missingCheckIn: Unrecognized parameter: code.');
-      resolve(false);
-    }
-  });
+// Function that, given an array of disconnectedUsers, takes the appropriate action and returns TRUE if action was successfully completed. Returns an error message if not.
+function handleDisconnectedUsers(data) {
+	return new Promise(function (resolve, reject) {
+		
+		let disconnectedUsers = data.disconnectedUsers;
+		let	users = data.users;
+		
+		// If number of disconnected users is 0:
+		if (disconnectedUsers.length === 0) {
+			// then there is nothing to handle, return TRUE
+			resolve(true);
+			
+		// If the number of disconnected users is not 0 and within the expected range:
+		} else if (disconnectedUsers.length >= 1 && disconnectedUsers.length <= maxGuests + 1) {
+			
+			// Determine the appropriate disconnect code.
+			writeDisconnectCode(data)
+			.then(function (disconnectCode) {
+				
+				// If the disconnect code is 'hostDisconnected' or 'notEnoughGuests,' update the lobby's connection status.
+				if (disconnectCode === 'hostDisconnected' || disconnectCode === 'notEnoughGuests') {
+					
+					// Attempt to update the connection status for the lobby in Firebase.
+					updateConnectionStatus(disconnectCode)
+					.then(function (updateComplete) {
+						// Return TRUE once complete.
+						resolve(true);
+					})
+					// Return an error if we are not able to update the lobby connection status.
+					.catch(function (error) {
+						reject(`handleDisconnectedUsers: error setting lobby connection status to disconnectCode (${disconnectCode}). Firebase error: ${error}.`);
+					});
+				// If the disconnect code is 'removeUsers':
+				} else if (disconnectCode === 'removeUsers') {
+					
+					// Run removeUsers, passing in the disconnectedUsers array.
+					removeUsers(disconnectedUsers)
+					
+					// Once the above is complete, return TRUE.
+					.then(function (updateComplete) {
+						resolve(true);
+					})
+					// If the above fails, return the error passed back from removeUsers.
+					.catch(function (error) {
+						reject(error);
+					});
+				// If the disconnectCode is neither of the above, it is unrecognized and we return an error.
+				} else {
+					reject(`handleDisconnectedUsers: disconnectCode (${disconnectCode}) is not recognized.`);
+				}
+			})
+		// Return an error if the number of disconnected users exceeds the range of what is possible for a lobby based on external variable maxGuests.
+		} else {
+			reject(`handleDisconnectedUsers: length of disconnectedUsers (${disconnectedUsers.length}) is outside of expected range (${maxGuests}).`);
+		}
+	});
 }
 
-let getCheckIns = new Promise(function(allUsersCheckedIn, missingCheckIn) {
-  const db = getDatabase();
-  const connectionRef = ref(db, 'rooms/TEST/connection');
-  const timestamp = Date.now();
-  
-  get(connectionRef).then((snapshot) => {
-    let hostUser = snapshot.val().host;
-    let allUsers = snapshot.val().users;
-    let totalUsers = 0;
-    let unresponsiveGuests = [];
-    for (let user in allUsers) {
-      if (allUsers[user].lastVerified  <= timestamp - verificationCadence) {
-        if (user === hostUser) {
-          missingCheckIn({
-        code: 'hostDisconnect',
-        guests: null
-      });
-        } else {
-          unresponsiveGuests.push(user);
-        }
-      }
-      totalUsers++;
-    }
-    if (unresponsiveGuests.length < 1) {
-      allUsersCheckedIn();
-    } else if (totalUsers - unresponsiveGuests.length - 1 < minimumGuests) {
-      missingCheckIn({
-        code: 'notEnoughGuests',
-        guests: null
-      });
-    } else {
-      console.log(unresponsiveGuests);
-      missingCheckIn({
-        code: 'removeGuests',
-        guests: unresponsiveGuests
-      });
-    }
-  });
+function checkForDisconnects() {
+	getConnectedUserData()
+	.then(function (responseTimes) {
+		return getDisconnectedUsers(responseTimes);
+	})
+	.then(function (data) {
+		return handleDisconnectedUsers(data);
+	})
+	.catch(error => {
+		console.log(error);
+	});
+}
+
+function verifyUser(uid) {
+	const db = getDatabase();
+	let timestamp = Date.now();
+	update(ref(db, `rooms/TEST/connection/users/${uid}`), {
+		lastVerified: timestamp
+	});
+}
+
+function anonSignIn () {
+	return newPromise(function (resolve, reject) {
+		const auth = getAuth();
+		
+		signInAnonymously(auth)
+		.then(() => {
+			resolve(auth);
+		}).catch((error) => {
+			reject(`anonSignIn: Firebase error: ${error}`);
+		});		
+	});
 });
 
-function verifyCheckIns () {
-  getCheckIns
-    .then(
-      function() {
-        console.log('all users checked in recently');
-      },
-      async function(obj) {
-        console.log(obj);
-        let code = obj.code;
-        let users = obj.guests;
-        
-        let handleMissingCheckIn = await missingCheckIn(code, users);
-        if (handleMissingCheckIn) {
-          console.log('Document ready: Missing check in was handled successfully.')
-        } else {
-          console.log('Document ready: Missing check in could not be handled.')
-        }
-      }
-    );
+function getNumUsers(roomcode) {
+	return new Promise(function (resolve, reject) {
+		const db = getDatabase();
+		get(ref(db, `rooms/${roomcode}/connection`)).then((snapshot) => {
+			let users = snapshot.val().users;
+			let numUsers = Number(users.length);
+			resolve(numUsers);
+		})
+		.catch((error) => {
+			reject(`getNumUsers: Firebase error: ${error}`);
+		});
+	});
 }
 
-$(document).ready(function () {
-  verifyCheckIns();
+function joinRoom(uid, roomcode) {
+	const db = getDatabase();
+	let updates = {};
+	let timestamp = Date.now();
+	let joinOrder = getNumUsers(roomcode);
+	let isHost = joinOrder === 0;
+	update[uid] = {
+		lastVerified: timestamp,
+		isHost = isHost,
+		joinOrder = joinOrder
+	};
+	update(ref(db, `rooms/${roomcode}/connection/users`), update).then(() => {
+	})
+	.catch((error) => {
+		console.log(`joinRoom: Firebase error: ${error}`);
+	});
+}
+
+$(document).ready(function() {
+	let currentUserSession;
+	let verificationInterval;
+	
+	anonSignIn()
+	.then((auth) => {
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				currentUserSession = new userSession(user.uid);
+				joinRoom(user.uid, 'TEST');
+				verificationInterval = setInterval(function(user.uid) {
+					verifyUser(user.uid);
+				}, ageAllowance);
+			} else {
+				console.log(`$(document).ready: user is signed out.`);
+				clearInterval(verificationInterval);
+			}
+		});
+	})
+	.catch(error => {
+		console.log(error);
+	});
 });
-
-/*
-function connectionCodeListener() {
-  const db = getDatabase();
-  const connectionCodeRef = ref(db, 'rooms/TEST/connection/connectionCode');
-  onValue(connectionCodeRef, (snapshot) => {
-    connectionCode = snapshot.val();
-    // Do something whenever this code changes.
-  });
-}
-
-let getUnverifiedUsers = new Promise(function(returnUsers) {
-  const db = getDatabase();
-  const connectedUsersRef = ref(db, 'rooms/TEST/connection/users');
-  const timeStamp = Date.now();
-  let users = [];
-  get(connectedUsersRef).then((snapshot) => {
-    snapshot.forEach((childSnapshot) => {
-      //console.log(childSnapshot.key); 
-      if (childSnapshot.val().lastVerified <= timeStamp - verificationCadence) {
-        users.push(childSnapshot.key);
-      } else {
-      }
-    });
-    returnUsers(users);
-  });
-});
-
-/*
-function reverifyUsers (users) {
-  const db = getDatabase();
-  const connectedUsersRef = ref(db, 'rooms/TEST/connection/users');
-  let unverified = users;
-  let verified = [];
-  unverified.forEach((user) => {
-    let timestamp = Date.now();
-    set(ref(db, `rooms/TEST/connection/users/${user}`), {
-      lastVerified: timestamp,
-      verificationStatus: 'pending'
-    })
-      .then(() => {
-        console.log('updated status');
-        verified.push(user);
-      })
-      .catch((error) => {
-    });
-  });
-}
-
-
-function verifyUser(db, user, timestamp) {
-  return new Promise(resolve => {
-    set(ref(db, `rooms/TEST/connection/users/${user}`), {
-      lastVerified: timestamp,
-      verificationStatus: 'pending'
-    })
-      .then(() => {
-        console.log('updated status');
-        resolve(user);
-      })
-      .catch((error) => {
-    });
-  });
-}
-
-
-
-function writeData(uid) {
-  const db = getDatabase();
-  console.log(uid);
-  set(ref(db, 'rooms/TEST'), {
-    host: uid
-  }).then(() => {
-  })
-  .catch((error) => {
-  });
-}
-// 
-
-function setConnectionCode (code) {
-  const db = getDatabase();
-  let connectionRef = ref(db, 'rooms/TEST/connection');
-  let codeUpdate = {};
-  codeUpdate['connectionCode'] = code;
-  update(ref(connectionRef), codeUpdate);
-}
-
-function updateVerificationTimestamp () {
-  const db = getDatabase();
-  let connectionRef = ref(db, 'rooms/TEST/connection');
-  let newTimestamp = {};
-  newTimestamp['lastVerified'] = Date.now();
-  update(ref(connectionRef), newTimestamp);
-}
-
-function removeGuests (guests) {
-  updateVerificationTimestamp ();
-}
-
-function handleUnresponsiveUsers (snapshot, unresponsiveUsers) {
-  return new Promise( function (setConnectionCode, removeGuests) {
-    let hostUser = snapshot.val().host;
-    if (unresponsiveUsers.includes(hostUser) {
-      setConnectionCode ('hostDisconnect');
-    } else {
-      let users = snapshot.val().users;
-      let numGuests = Number(users.length) - 1;
-      if (numGuests - users.length < minimumGuests) {
-        setConnectionCode ('notEnoughPlayers');
-      } else {
-        removeGuests (unresponsiveUsers);
-      }
-    }
-  });
-});
-
-async function getUsersWithStatus(snapshot, status) {
-  let users = [];
-  for (let childSnapshot of snapshot) {
-    if (childSnapshot.val().verificationStatus === status) {
-      users.push(childSnapshot.key);
-    } else {
-    }
-  }
-  return users;
-  });
-}
-
-function checkVerificationStatus (snapshot) {
-  return new Promise ( function (handleUnresponsiveUsers, updateVerificationTimestamp) {
-    let hasPending = [];
-    let hasConfirmed = [];
-    hasPending = await getUsersWithStatus (snapshot, 'pending');
-    hasConfirmed = await getUsersWithStatus (snapshot, 'confirmed');
-    if (hasPending.length < 1) {
-      updateVerificationTimestamp ();
-    } else {
-      handleUnresponsiveUsers (snapshot, hasPending);
-    }
-  });
-}
-
-function getVerificationTimestamp () {
-  return new Promise(function (checkVerificationStatus, recentlyVerified) {
-    const db = getDatabase();
-    const connectionRef = ref(db, 'rooms/TEST/connection');
-    const timeStamp = Date.now();
-    get(connectionRef).then((snapshot) => {
-        if (snapshot.val().lastVerified <= timeStamp - verificationCadence) {
-          checkVerificationStatus(snapshot);
-        } else {
-          recentlyVerified ();
-        }
-    });
-  });
-}
-
-let checkVerification = new Promise(function() {
-});
-
-$(document).ready(function () {
-  //connectionCodeListener();
-  //getUnverifiedUsers();
-  //getUnverifiedUsers.then(
-  //  function(users) {
-  //    console.log(users);
-  //    setPendingStatus(users);
-  //  }
-  //);
-  getVerificationTimestamp ().then(
-    function () {
-      console.log('Verification needed');
-      checkVerificationStatus (snapshot).then(
-        function () {
-          console.log('Unresponsive users found');
-          handleUnresponsiveUsers (snapshot, unresponsiveUsers).then(
-            function () {
-              console.log('Game-ending condition from disconnect');
-              setConnectionCode (code);
-            },
-            function () {
-              console.log('Game can continue after player(s) are removed');
-              removeGuests (guests);
-            }
-          );
-        },
-        function () {
-          console.log('All users responded');
-          updateVerificationTimestamp ();
-        }
-      );
-    },
-    function () {
-      console.log('No verification needed');
-    }
-  );
-});
-*/
